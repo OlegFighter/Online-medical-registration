f16616657833f73282c1ce7b19f9407b6787cb3c
MickeyShide <mickey_shide@list.ru>
2023-10-21 23:34:06 +0300

    MY COMMIT AAAA

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main() {
 	cin >> name;
 	cout << "Hello, " << name << "!\n";
 	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	cout << "Nice to see you learning Git\n";
+	
 	system("pause");
 	return 0;
 }
